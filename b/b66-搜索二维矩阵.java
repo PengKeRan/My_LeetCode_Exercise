da7b893754c7bdb7106fbed9cb5fee2a977922fc
pengkr <1353273250@qq.com>
2024-09-25 21:32:59 +0800

    9-25

---
+++ b/b66-搜索二维矩阵.java
@@ -0,0 +1,34 @@
+// 240. 搜索二维矩阵 II
+// 编写一个高效的算法来搜索 m x n 矩阵 matrix 中的一个目标值 target 。该矩阵具有以下特性：
+
+// 每行的元素从左到右升序排列。
+// 每列的元素从上到下升序排列。
+
+class SearchMatrix {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix.length == 0 || matrix[0].length == 0) {
+            return false;
+        }
+        int i = 0, j = matrix[0].length - 1;
+        while (i < matrix.length && j >= 0) {
+            if (matrix[i][j] == target) {
+                return true;
+            }
+            if (matrix[i][j] > target) {
+                j--;
+            } else {
+                i++;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        SearchMatrix sol = new SearchMatrix();
+        int[][] matrix = { { 1, 4, 7, 11, 15 }, { 2, 5, 8, 12, 19 }, { 3, 6, 9, 16, 22 }, { 10, 13, 14, 17, 24 },
+                { 18, 21, 23, 26, 30 } };
+        int target = 26;
+        System.out.println(sol.searchMatrix(matrix, target));
+        // System.out.println(sol.decodeString(s2));
+    }
+}

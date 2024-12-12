930e1b71eaf3cf1b0673f17c08b4f35c1fe9e1ca
pengkr <1353273250@qq.com>
2024-12-12 20:58:17 +0800

    12-12-3

--- a/a43-找出字符串中第一个匹配项的下标.py
+++ a/a43-找出字符串中第一个匹配项的下标.py
@@ -11,26 +11,26 @@ class Solution(object):
         :rtype: int
         """
         # KMP
-        # n = len(needle)
-        # next_arr = [0] * n
-        # j = 0  # j表示最长前缀后缀的长度
-        # for i in range(1, n):
-        #     while j > 0 and needle[i] != needle[j]:
-        #         j = next_arr[j - 1]  # 如果字符不匹配，就回溯到前一个最长前后缀的索引
-        #     if needle[i] == needle[j]:
-        #         j += 1
-        #     next_arr[i] = j
+        n = len(needle)
+        next_arr = [0] * n
+        j = 0  # j表示最长前缀后缀的长度
+        for i in range(1, n):
+            while j > 0 and needle[i] != needle[j]:
+                j = next_arr[j - 1]  # 如果字符不匹配，就回溯到前一个最长前后缀的索引
+            if needle[i] == needle[j]:
+                j += 1
+            next_arr[i] = j
 
-        # j = 0
-        # for i in range(len(haystack)):
-        #     while j > 0 and haystack[i] != needle[j]:
-        #         j = next_arr[j - 1]
-        #     if haystack[i] == needle[j]:
-        #         j += 1
-        #     if j == n:
-        #         return i - n + 1
+        j = 0
+        for i in range(len(haystack)):
+            while j > 0 and haystack[i] != needle[j]:
+                j = next_arr[j - 1]
+            if haystack[i] == needle[j]:
+                j += 1
+            if j == n:
+                return i - n + 1
 
-        # return -1
+        return -1
 
         # 动态规划
         # n = len(haystack)
@@ -45,19 +45,6 @@ class Solution(object):
         #         if dp[i][j] == m:
         #             return i - m
         # return -1
-
-        n = len(haystack)
-        m = len(needle)
-        dp = [[0 for _ in range(m + 1)] for _ in range(n + 1)]
-        for i in range(1, n + 1):
-            for j in range(1, m + 1):
-                if haystack[i - 1] == needle[j - 1]:
-                    dp[i][j] = dp[i - 1][j - 1] + 1
-                else:
-                    dp[i][j] == 0
-                if dp[i][j] == m:
-                    return i - m
-        return -1
 
 
 sol = Solution()

5489382dfc074c0410a3bf051a1f72c87837fe1e
pengkr <1353273250@qq.com>
2024-12-26 16:56:16 +0800

    12-26-3

---
+++ b/b120-插入区间.py
@@ -0,0 +1,29 @@
+# 57. 插入区间
+class Solution(object):
+    def insert(self, intervals, newInterval):
+        """
+        :type intervals: List[List[int]]
+        :type newInterval: List[int]
+        :rtype: List[List[int]]
+        """
+        res = []
+        n = len(intervals)
+        i = 0
+        while i < n and intervals[i][1] < newInterval[0]:
+            res.append(intervals[i])
+            i += 1
+        while i < n and intervals[i][0] <= newInterval[1]:
+            newInterval[0] = min(intervals[i][0], newInterval[0])
+            newInterval[1] = max(newInterval[1], intervals[i][1])
+            i += 1
+        res.append(newInterval)
+        while i < n:
+            res.append(intervals[i])
+            i += 1
+        return res
+
+
+intervals = [[1, 3], [6, 9]]
+newInterval = [2, 5]
+sol = Solution()
+print(sol.insert(intervals, newInterval))

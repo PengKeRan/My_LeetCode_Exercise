9a25d56ebc9adc0f8ccff2e5fcf0a67309e26ca9
pengkr <1353273250@qq.com>
2025-02-19 21:51:56 +0800

    2-19-2

---
+++ a/a69-位1的个数.java
@@ -0,0 +1,12 @@
+class HammingWeight {
+    public int hammingWeight(int n) {
+        int res = 0;
+        while (n != 0) {
+            if ((n & 1) == 1) {
+                res += 1;
+            }
+            n = n >> 1;
+        }
+        return res;
+    }
+}

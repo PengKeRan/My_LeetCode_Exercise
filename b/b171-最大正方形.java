c80961d29e744af275f3c452c66e06868d4d9027
pengkr <1353273250@qq.com>
2025-02-26 17:18:58 +0800

    2-26-1

---
+++ b/b171-最大正方形.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+        int ans = 0;
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = matrix[i][j] == '1' ? 1 : 0;
+                } else {
+                    if (matrix[i][j] == '1') {
+                        dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;
+                    } else {
+                        dp[i][j] = 0;
+                    }
+                }
+                ans = Math.max(ans, dp[i][j]);
+            }
+        }
+        return ans * ans;
+    }
+}
